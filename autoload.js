573c310b80eaa4ee4919ece46b57a8b67351f218
zhonghuatu <39081637+zhonghuatu@users.noreply.github.com>
2019-08-16 22:05:25 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 //注意：live2d_path参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/zhonghuatu/live2d-widget@0.3.1/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/zhonghuatu/live2d-widget@0.3.3/";
 //const live2d_path = "/live2d-widget/";
 
 //加载waifu.css
